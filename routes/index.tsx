e87f42fa2be13ee350773931df1dcac6a2f78a1c
sagyobake <sagyobake@localhost.localdomain>
2025-03-19 23:27:26 +0900

    upgrade

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,47 +1,18 @@
-const heartFunc = () => { // nはハートの模様を変えるのに用いる引数
-    const array = [];
-
-    const e = Math.E;
-
-    for (let x = -e; x <= e; x += 0.0002) {
-        const y =
-            Math.sin(e ** 11 * x) *
-            Math.sqrt((e ** 2 - x ** 2) / 1.8) +
-            Math.log(Math.abs(x) + 0.7);
-
-        array.push(
-            <div
-                class="dot"
-                style={{
-                    left: `${50 + (16 * x)}dvmin`,
-                    top: `${80 + (-1 * (16 * y))}dvmin`,
-                }}
-            >
-            </div>,
-        );
-    }
-
-    console.log(array.length);
-
-    return array;
-};
-
 export default function Home() {
 
     return (
         <body>
             <header>
-                <h1>
-                    Kindle執筆で新世界の神になる！
-                </h1>
+                <h1>内田まい</h1>
                 <i>若者いじめて楽しいか?</i>
             </header>
 
             <main>
-                {heartFunc()}
+                <img src="https://pbs.twimg.com/profile_images/1899763838154944512/VOxmFhSg_400x400.jpg"></img>
             </main>
 
             <footer>
+
             </footer>
 
         </body>
